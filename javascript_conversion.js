740830a042068ad0c4965a7cef360f22ad2d1a44
Graham Spencer <grahamspence@gmail.com>
2018-09-05 12:18:49 -0400

    steps 1 + 2 complete

---
+++ javascript_conversion.js
@@ -0,0 +1,13 @@
+apples = 14;
+console.log('I have ' + apples + ' apples.');
+
+/////////////////////////////////////////////////
+
+var materials = ['wood', 'metal', 'stone'];
+var words = {
+  'elephant':"The world's largest land mammal.",
+  'school':'A place of learning.',
+  'ice cream':'A delicious milk-based dessert.'
+}
+console.log(materials)
+console.log(words)
